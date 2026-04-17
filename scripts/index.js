728597e54e37b5238c857646646e827734fafdcb
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-04-17 14:05:04 +0200

    completata visualizzazione a schermo dei dati tramite innerHTML

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 
 const apiUrl = 'https://lanciweb.github.io/demo/api/pictures/';
 
+const fotoWrapperEl = document.querySelector('.foto-wrapper');
+const cardWrapperEl = document.querySelector('.card-wrapper');
+
 /* let esempio = {id: 1, title: 'Skate Park', date: '01-07-2024',
      url: 'https://marcolanci.it/boolean/assets/pictures/1.png'} */
 
@@ -10,6 +13,21 @@ fetch(apiUrl)
     }).then(album => {
         album.forEach(foto => { //questa sarà la mia funzione di caricamento poi
             console.log(foto);
+            
+            cardWrapperEl.innerHTML += `
+                                        <div class="my-card col-12 col-md-6 col-lg-4">
+                                            <div class="pin-wrapper">
+                                                <img class="pin" src="img/pin.svg" alt="pin">
+                                            </div>
+                                            <div class="foto-wrapper">
+                                                <img class="img-fluid" src="${foto.url}" alt="">
+                                            </div>
+                                            <div class="stats-wrapper">
+                                                <p class="foto-data font-date">${foto.date}</p>
+                                                <p class="foto-title font-titles">${foto.title}</p>
+                                            </div>
+                                        </div>
+            `
         });
     }).catch(error => {
         console.error('errore');

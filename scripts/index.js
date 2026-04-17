cad9f64f1e13d58770746a272fba434078275e2c
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-04-17 11:50:23 +0200

    milestone 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
-console.log('ciao');
+
+const apiUrl = 'https://lanciweb.github.io/demo/api/pictures/';
+
+/* let esempio = {id: 1, title: 'Skate Park', date: '01-07-2024',
+     url: 'https://marcolanci.it/boolean/assets/pictures/1.png'} */
+
+fetch(apiUrl)
+    .then(response => {
+        return response.json(); //è un array di oggetti
+    }).then(album => {
+        album.forEach(foto => { //questa sarà la mia funzione di caricamento poi
+            console.log(foto);
+        });
+    }).catch(error => {
+        console.error('errore');
+    })

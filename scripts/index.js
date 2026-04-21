0b5b8aa2f5a5f700a8a3796a0a7e4fbe43ad71cf
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-04-21 16:06:50 +0200

    aggiunto carosello nell'overlay

--- scripts/index.js
+++ scripts/index.js
@@ -7,19 +7,26 @@ const overlayImgEl = document.querySelector('.overlay-img');
 const overlayContainer = document.querySelector('.overlay-container');
 const chiudiBtn = document.querySelector('.chiudi-btn');
 const body = document.querySelector('.body');
+const backBtn = document.querySelector('.back-btn');
+const nextBtn = document.querySelector('.next-btn');
 
 /* let esempio = {id: 1, title: 'Skate Park', date: '01-07-2024',
      url: 'https://marcolanci.it/boolean/assets/pictures/1.png'} */
+
+let albumSaved = [];
+
+
 
 fetch(apiUrl)
     .then(response => {
         return response.json(); //è un array di oggetti
     }).then(album => {
         album.forEach(foto => { //questa sarà la mia funzione di caricamento poi
-            console.log(foto);
-            
+            albumSaved.push(foto)
+
+
             cardWrapperEl.innerHTML += `
-                                        <div class="my-card col-12 col-md-6 col-lg-4" data-pic="${foto.url}">
+                                        <div class="my-card col-12 col-md-6 col-lg-4" data-pic="${foto.url}" data-id="${foto.id}">
                                             <div class="pin-wrapper">
                                                 <img class="pin" src="img/pin.svg" alt="pin">
                                             </div>
@@ -32,28 +39,74 @@ fetch(apiUrl)
                                             </div>
                                         </div>
             `
+
         });
+        return album;
     }).catch(error => {
         console.error('errore');
+    }).then(el => {
+        console.log(albumSaved);
     })
 
-function clickOnCard(event){
-    const target = event.target
+
+let currentId = 0;
+
+function clickOnCard(event) {
+
+    const target = event.target;
     const overlayImage = target.closest('.my-card').dataset.pic;
-    
+    const targetId = target.closest('.my-card').dataset.id;
+    console.log(targetId);
+
+
 
     overlayContainer.classList.remove('d-none');
-    body.classList.add('overflow-hidden');
 
-    if (target.closest('.my-card') !== null){
+
+    if (target.closest('.my-card') !== null) {
         overlayImgEl.src = `${overlayImage}`;
+        body.classList.add('overflow-hidden');
+        currentId = Number(targetId) - 1;
+        return targetId;
+    }
+
+}
+
+function dnoneOverlay() {
+    overlayContainer.classList.add('d-none');
+    body.classList.remove('overflow-hidden');
+}
+
+function nextPic() {
+    currentId += 1;
+
+    if (albumSaved[currentId] !== undefined){
+        
+        overlayImgEl.src = `${albumSaved[currentId].url}`;
+        console.log(albumSaved[currentId].id);
+        
+    } else {
+        currentId = 0;
+        overlayImgEl.src = `${albumSaved[currentId].url}`;
+        console.log(albumSaved[currentId].id);
     }
     
 }
 
-function dnoneOverlay(){
-    overlayContainer.classList.add('d-none');
-    body.classList.remove('overflow-hidden');
+function previousPic() {
+    currentId -= 1;
+
+    if (albumSaved[currentId] !== undefined){
+        
+        overlayImgEl.src = `${albumSaved[currentId].url}`;
+        console.log(albumSaved[currentId].id);
+        
+    } else {
+        currentId = albumSaved.length - 1;
+        overlayImgEl.src = `${albumSaved[currentId].url}`;
+        console.log(albumSaved[currentId].id);
+    }
+    
 }
 
 cardWrapperEl.addEventListener(
@@ -62,4 +115,12 @@ cardWrapperEl.addEventListener(
 
 chiudiBtn.addEventListener(
     'click', dnoneOverlay
+)
+
+nextBtn.addEventListener(
+    'click', nextPic
+)
+
+backBtn.addEventListener(
+    'click', previousPic
 )
